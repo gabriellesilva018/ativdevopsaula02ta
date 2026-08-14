7886f6144586c12cdc3d02eb92dc25ccdd7be0cb
bruninxsz <gabriellejuliaa319@gmail.com>
2026-08-13 22:37:57 -0300

    Aula 02 Devops primeira pipeline

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main { 
+
+    public static void Main (String´[] args) {
+        System.out.println(x: "OLA, DEVOPS");
+        System.out.println(x: "PIPELINE EXECUTADA COM SUCESSO1");        
+
+    }
+}
